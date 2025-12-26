5435536fdce594165be30cef640b8a6c653de293
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2025-12-26 22:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+    const select = document.getElementById("colorSelect");
+
+    // Remove the selected option
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+}
